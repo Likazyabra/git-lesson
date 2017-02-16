3c8dea3c075fbb0625d34b3a177898b7d1de5d77
Angelika Varenik <angelika.varenik@gmail.com>
2017-02-17 00:40:30 +0300

    Первое задание

--- 1.py
+++ 1.py
@@ -1,5 +1,7 @@
-print("Hi")
-t = 4
-d = 2
-fsss
-123
+h = [i for i in range(1,61,1)]
+green = sorted(h[::6] + h[1::6] + h[2::6])
+from datetime import datetime
+if datetime.now().minute in green:
+    print('green')
+else:
+    print('red')
