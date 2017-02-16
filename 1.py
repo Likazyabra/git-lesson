87c19b4151eae94a445e24fdc3cad0b54c5b894a
Angelika Varenik <angelika.varenik@gmail.com>
2017-02-17 00:48:22 +0300

    Первое задание

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 h = [i for i in range(1,61,1)]
-green = sorted(h[::6] + h[1::6] + h[2::6])
+green = sorted(h[::5] + h[1::5] + h[2::5])
 from datetime import datetime
 if datetime.now().minute in green:
     print('green')
