d6e2fa1042e86e9a9028439a2901dbb2f7dbd00f
Angelika Varenik <angelika.varenik@gmail.com>
2017-02-20 23:23:01 +0300

    Второе задание

--- 6.py
+++ 6.py
@@ -1,4 +1,19 @@
-lst = [11, 2, 3, 8, 14, 89, 45]
+lst = [1, 11, 2, 3, 8, 9, 14, 89, 45]
+
+len_lst = len(lst) - 1
+j = len_lst
+
+while j >= 0:
+    if j < len_lst/2:
+        break
+
+    lst[len_lst - j], lst[j] = lst[j], lst[len_lst - j]
+    j -= 1
+
+print(lst)
+
+# Это я погорячилась :)
+"""lst = [11, 2, 3, 8, 14, 89, 45]
 len_lst = len(lst) - 1
 j = len_lst
 
@@ -17,4 +32,4 @@ while i <= len_lst:
     lst[i] = int(float(lst[i]))
     i += 1
     
-print(lst)
+print(lst)"""
