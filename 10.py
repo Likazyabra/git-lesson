643ab1a49684e0c1c38010541ed8ad8afebda6a3
Angelika Varenik <angelika.varenik@gmail.com>
2017-02-26 19:40:01 +0300

    Третье задание 27.02 Номер четверти

--- 10.py
+++ 10.py
@@ -1,12 +1,14 @@
 def area_number(x, y):
+    
     if x == 0 and y == 0:
         return '0'
-
-    if x >= 0 and y >= 0:
+    elif x == 0 or y == 0:
+        return '01'
+    elif x > 0 and y > 0:
         return '1'
-    elif x <= 0 and y >= 0:
+    elif x < 0 and y > 0:
         return '2'
-    elif x <= 0 and y <= 0:
+    elif x < 0 and y < 0:
         return '3'
     else:
         return '4'
@@ -19,5 +21,7 @@ area = area_number(x, y)
 
 if area == '0':
     print('Это начало координат')
+elif area == '01':
+    print('Точка находится на оси координат')
 else:
     print('Точка принадлежит {} четверти'.format(area))
